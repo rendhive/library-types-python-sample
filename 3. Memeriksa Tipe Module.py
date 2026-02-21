7cdddce32ed86a40c03ad8195f5276d3df8ea983
rendhive <rendhivecode@gmail.com>
2026-02-21 23:35:05 +0700

    [DOCT] Sample Library Python: 3. Memeriksa Tipe Module.py

---
+++ 3. Memeriksa Tipe Module.py
@@ -0,0 +1,6 @@
+import types
+import math
+
+print(isinstance(math, types.ModuleType))  # Memeriksa apakah ini adalah module
+# Fungsi: Memeriksa apakah objek adalah module.
+# Kondisi: Ketika Anda ingin memverifikasi bahwa objek adalah sebuah module.
