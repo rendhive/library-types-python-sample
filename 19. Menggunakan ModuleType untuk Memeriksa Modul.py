ffa659e193ad29fa292fc66d037da9f25ff71fd3
rendhive <rendhivecode@gmail.com>
2026-02-21 23:39:48 +0700

    [DOCT] Sample Library Python: 19. Menggunakan ModuleType untuk Memeriksa Modul.py

---
+++ 19. Menggunakan ModuleType untuk Memeriksa Modul.py
@@ -0,0 +1,5 @@
+import types
+
+print(isinstance(types, types.ModuleType))  # Memeriksa apakah `types` adalah modul
+# Fungsi: Memeriksa apakah objek adalah module.
+# Kondisi: Ketika Anda ingin menjalankan verifikasi pada objek type module.
