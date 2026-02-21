910d1bb9ccd83123e2f40feae7ff1cac40935fc1
rendhive <rendhivecode@gmail.com>
2026-02-21 23:35:20 +0700

    [DOCT] Sample Library Python: 4. Memeriksa Tipe Class.py

---
+++ 4. Memeriksa Tipe Class.py
@@ -0,0 +1,8 @@
+import types
+
+class SampleClass:
+    pass
+
+print(isinstance(SampleClass, types.TypeType))  # Memeriksa apakah ini adalah kelas
+# Fungsi: Memeriksa apakah objek adalah kelas (tipe).
+# Kondisi: Ketika Anda ingin memverifikasi bahwa objek adalah kelas.
