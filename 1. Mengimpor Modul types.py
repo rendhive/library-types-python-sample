d67c9cfa1609ae0a54daa944991f7cb18e085832
rendhive <rendhivecode@gmail.com>
2026-02-21 23:19:25 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul types.py

---
+++ 1. Mengimpor Modul types.py
@@ -0,0 +1,5 @@
+import types
+
+print("Modul types berhasil diimpor.")
+# Fungsi: Mengimpor modul types untuk menggunakan fitur-fitur tipe objek.
+# Kondisi: Digunakan sebelum memanfaatkan fungsi dan objek dari modul types.
