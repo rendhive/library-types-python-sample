8b8939b41c413133bff948651073b1d5141a5c22
rendhive <rendhivecode@gmail.com>
2026-02-21 23:34:48 +0700

    [DOCT] Sample Library Python: 2. Memeriksa Tipe Fungsi.py

---
+++ 2. Memeriksa Tipe Fungsi.py
@@ -0,0 +1,8 @@
+import types
+
+def sample_function():
+    pass
+
+print(isinstance(sample_function, types.FunctionType))  # Memeriksa apakah ini adalah fungsi
+# Fungsi: Memeriksa apakah objek adalah fungsi.
+# Kondisi: Ketika Anda ingin memastikan objek adalah fungsi sebelum memanggilnya.
