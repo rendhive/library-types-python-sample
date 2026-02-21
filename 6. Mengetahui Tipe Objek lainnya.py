bf84482aaa2c88f50e96817bf21ec0494f0bc947
rendhive <rendhivecode@gmail.com>
2026-02-21 23:35:56 +0700

    [DOCT] Sample Library Python:6. Mengetahui Tipe Objek lainnya.py

---
+++ 6. Mengetahui Tipe Objek lainnya.py
@@ -0,0 +1,5 @@
+import types
+
+print(types.StringTypes)  # Menampilkan tipe string
+# Fungsi: Menyediakan informasi tentang tipe objek di dalam modul.
+# Kondisi: Ketika Anda ingin menggunakan tipe informasi tentang objek yang ada di modul.
